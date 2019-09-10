6a5e933bde0b663cba758dd55a2e2b21b59bf018
zcg <1146009494@qq.com>
2019-09-10 21:01:01 +0800

    新增对于生产者的事务与签收参数的理解

--- src/main/java/JMSProducer.java
+++ src/main/java/JMSProducer.java
@@ -18,18 +18,25 @@ public class JMSProducer {
         //获取连接并启动
         Connection connection = activeMQConnectionFactory.createConnection();
         connection.start();
-        //创建会话
-        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        /**
+         *  创建会话,true为开启事务，需要commit提交之后才能进入队列,false 表示send直接发送到中间件
+         */
+        Session session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
         //创建目的地也就是消息中间件
         Queue queue = session.createQueue(MYQUEUE);
         //创建消息生产者
         MessageProducer messageProducer = session.createProducer(queue);
+
+        //messageProducer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);//设置消息非持久化,MQ宕机消息永久丢失
+        //messageProducer.setDeliveryMode(DeliveryMode.PERSISTENT);//设置消息持久化，默认消息持久化
 
         //发布消息
         for (int i = 1; i <= 3; i++) {
             TextMessage textMessage = session.createTextMessage("msg----" + i);
             messageProducer.send(textMessage);
         }
+        session.commit();
         //释放资源
         messageProducer.close();
         session.close();
